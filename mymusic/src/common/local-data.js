90e81894f23a378ae511b89ab9f6aa8a8145027b
hanzhenzhen-git <1098874458@qq.com>
2022-04-21 18:13:14 +0800

    addfooter

--- mymusic/src/common/local-data.js
+++ mymusic/src/common/local-data.js
@@ -24,3 +24,26 @@ export const headerLinks = [
         link: "http://music.163.com/#/download"
     },
 ]
+
+export const footerLinks = [
+    {
+        title: "服务条款",
+        link: "https://st.music.163.com/official-terms/service"
+    },
+    {
+        title: "隐私政策",
+        link: "https://st.music.163.com/official-terms/privacy"
+    },
+    {
+        title: "儿童隐私政策",
+        link: "https://st.music.163.com/official-terms/children"
+    },
+    {
+        title: "版权投诉指引",
+        link: "https://music.163.com/st/staticdeal/complaints.html"
+    },
+    {
+        title: "意见反馈",
+        link: "#"
+    }
+]
---
+++ mymusic/src/components/app-footer/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const AppFooterWrapper = styled.div`
+    
+`
+
+export const FooterLeft = styled.div`
+    
+`
+
+export const FooterRight = styled.div`
+    
+`
